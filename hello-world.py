ea83eedc832914956e94871900e678e670ddcd11
Randy Miller <randy918@gmail.com>
2022-03-20 13:35:27 -0500

    lesson 74 python elseif

--- hello-world.py
+++ hello-world.py
@@ -1,9 +1,26 @@
-from random import choice
-food = choice(['apple','grape', 'bacon', 'steak', 'worm', 'dirt'])
+# NO TOUCHING==NO TOUCHING==NO TOUCHING==NO TOUCHING #| \
+from random import randint                           #|  \
+x = randint(-100, 100)                               #|   \
+while x == 0:  # make sure x isn't zero              #|    \
+    x = randint(-100, 100)                           #|     NO TOUCHING!!!!!! (please)         
+y = randint(-100, 100)                               #|    /
+while y == 0:  # make sure y isn't zero              #|   /
+    y = randint(-100, 100)                           #|  /
+# NO TOUCHING==NO TOUCHING==NO TOUCHING==NO TOUCHING #| /
 
+if x > 0 and y > 0:
+	print("both positive")
+elif x < 0:
+	print("x is negative")
+else:
+	print("y is negative")
 
-if food == 'bacon' or food == 'steak':
-    print("meat")
-elif food == 'apple' or food == 'grape':
-    print("fruit")
-else: print("yuck")
+if x  <  0 and y  <  0:
+	print("both negative")
+elif x > 0:
+	print("x is positive")
+else:
+	print("y is positive")
+
+print(x, y)
+
