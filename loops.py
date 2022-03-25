c273696cc186cf5241b8ab85512a31e887efb32f
Randy Miller <randy918@gmail.com>
2022-03-24 19:49:38 -0500

    now loops 2

--- loops.py
+++ loops.py
@@ -7,58 +7,8 @@ from random import randint
 def restart():
     os.execl(sys.executable, sys.executable, *sys.argv)
 
-
-def screen_drop(substance):
-    if substance != "rock" and substance != "scissors" and substance != "paper":
-        print("not acceptable\n")
-        restart()
-    else:
-        print("Thank you\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+for x in range(9, 99):
+    print(x)
+#restart()
 
 
-substances = ["rock", "scissors", "paper"]
-
-# ROCK = 1
-# SCISSORS = 2
-# PAPER = 3
-
-
-
-print("...rock...")
-print("...paper...")
-print("...scissors...")
-
-player1 = input("(enter Player 1's choice): ")
-screen_drop(player1)
-
-
-print("SHOOT!")
-
-player2 = substances[randint(0, 2)]
-print(f"Player 1 plays {player1} and Player 2 plays {player2}")
-
-if player1 == player2:
-    print("no winner")
-    restart()
-
-if player1 == "rock":
-    if player2 == "scissors":
-        winner = 1
-    else:
-        winner = 2
-
-if player1 == "scissors":
-    if player2 == "paper":
-        winner = 1
-    else:
-        winner = 2
-
-if player1 == "paper":
-    if player2 == "rock":
-        winner = 1
-    else:
-        winner = 2
-
-print(f"Winner is player{winner}")
-
-restart()
