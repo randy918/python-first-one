68afc0c59c60279d002fb2a29a30f62ec21451d3
Randy Miller <randy918@gmail.com>
2022-04-02 14:55:33 -0500

    1-20 test

--- loops.py
+++ loops.py
@@ -7,8 +7,22 @@ from random import randint
 def restart():
     os.execl(sys.executable, sys.executable, *sys.argv)
 
-for x in range(9, 99):
-    print(x)
+# Add up all odd numbers between 10 and 20
+# Store the result in x:
+for x in range(1,21):
+    if (x == 4) != (x == 13):
+        print(x,"unlucky")
+    elif x % 2 == 1:
+        print(x,"odd")
+    else:
+        print(x,"even")
+
+
+
 #restart()
+
+
+
+
 
 
