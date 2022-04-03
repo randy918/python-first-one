121ea6c19fa7a6a3dc94193aadbacfcd9a5a2fa3
Randy Miller <randy918@gmail.com>
2022-04-03 08:55:43 -0500

    93 Guessing Game success

--- loops.py
+++ loops.py
@@ -2,28 +2,43 @@ import os
 import sys
 from random import randint
 
-
-
 def restart():
     os.execl(sys.executable, sys.executable, *sys.argv)
 
-smiles = "\U0001f600"
+def get_guess():
+    global received
+    received = input(starter + " ")
+    if str.isdigit(received) and int(received) >= LOW and int(received) <= HIGH:
+        received = int(received)
+    else:
+        print("Try again.")
+        get_guess()
 
-lines = 9
-
-for x in range(lines + 1):
-    markers = lines - x
-    print("")
-    while markers < lines:
-        print(smiles, end = " " )
-        markers += 1
-# while lines < 10:
-#     print("")
+def assess_guess(received):
+    if received == number:
+        print("Correct!")
+        restart()
+    elif received < number:
+        print("Too Low")
+    elif received > number:
+        print("Too High")
 
 
+LOW = 1
+HIGH = 10
+number = randint(LOW, HIGH)
+starter = f"\nGuess a number from {LOW} to {HIGH}:"
+
+nowin = True
+
+while nowin:
+    get_guess()
+    assess_guess(received)
 
 
-#restart()
+#print(received, number)
+
+restart()
 
 
 
