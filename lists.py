09e7412c75a7deb2b08c1f2a4dc113a51f1753a7
Randy Miller <randy918@gmail.com>
2022-04-03 15:04:11 -0500

    python list practice

--- lists.py
+++ lists.py
@@ -7,5 +7,33 @@ def restart():
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
-print("hh")
+demo_list = ["trial", "final", "hub"]
+print("\n")
+print(demo_list)
+print(len(demo_list))
+
 # restart()
+
+
+people = ["Hanna", "Louisa", "Claudia", "Angela", "Geoffrey", "aparna"]
+
+for x in range(len(people)):
+    if people[x] == "Hanna":
+        people[x] = people[x] + "h"
+    if people[x] == "Geoffrey":
+        people[x] = "Jeffrey"
+    if people[x] == "aparna":
+        people[x] = "Arpana"
+
+    print(people[x])
+
+    numerics = ("one", "two", "three", "four")
+print(numerics)
+
+for x in numerics:
+    print(x)
+
+i = 0
+while i < len(numerics):
+    print(numerics[i])
+    i += 1
